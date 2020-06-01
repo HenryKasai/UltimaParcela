20dd2a2b794add9c21cfba0d6ea9cf38ddfa27e3
HenryKasai <henrykasai1775@gmail.com>
2020-06-01 19:31:47 -0300

    Exercício Resolvido.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UltimaParcela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Insira o valor total a ser pago.");
+            
+            string vlrtotal = Console.ReadLine();
+            decimal vlrtotaldecimal = Convert.ToDecimal(vlrtotal);
+
+            Console.WriteLine("Insira a quantidade total de parcelas mensais (sem juros)");
+
+            string parcelas = Console.ReadLine();
+            int parcelasint = Convert.ToInt32(parcelas);
+            decimal vlrparcela = vlrtotaldecimal / parcelasint;
+            DateTime agora = DateTime.Now;
+            DateTime final = agora.AddMonths(parcelasint);
+            string finalstring = Convert.ToString(final);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Sua última parcela será em {final.ToShortDateString()}, no valor de R${vlrparcela:N2}.");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+        }
+    }
+}
